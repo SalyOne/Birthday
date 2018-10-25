125a1808aff48988f5996956d553b9d3ac8eea33
Salome Batkhadze <salome.batkhadze@geolab.edu.ge>
2018-10-25 21:13:38 +0400

    finished:

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let tl= new TimelineMax();
 let cont=document.querySelector(".container");
 
-tl.from('.box-bottom', 2,{ scale:0.5 ,transformOrigin:"50% 50%",ease: Bounce.easeOut})
+
+tl.from('.box-bottom , .box-hat', 2,{ scale:0.5 ,transformOrigin:"50% 50%",ease: Bounce.easeOut})
 .to('.box-hat', 2,{ scale:0.9,rotation:35, y:-50,transformOrigin:"50% 50%",ease: Bounce.easeOut})
-.to('.small', 2,{ scale:0.9,rotation:35, y:-50,transformOrigin:"50% 50%",ease: Bounce.easeOut})
+.staggerFromTo('.text', 2,{ scale:0.5,y:0,transformOrigin:"50% 50%",ease: Bounce.easeOut},
+					{ scale:1.5,y:-30,transformOrigin:"50% 50%",ease: Bounce.easeOut})
 
